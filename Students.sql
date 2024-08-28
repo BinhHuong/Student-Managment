e4c9012b9292bf0a7745b5c91fb01475ccc0fa8e
BinhHuong <129530935+BinhHuong@users.noreply.github.com>
2024-08-28 15:33:21 +0700

    Add files via upload

---
+++ Students.sql
@@ -0,0 +1,10 @@
+
+CREATE TABLE Students(
+	StudentID int NOT NULL PRIMARY KEY,
+	FullName varchar(50) NULL,
+	DateOfBirth date NULL,
+	Address varchar(100) NULL,
+	ContactNumber varchar(15) NULL,
+	Email varchar(50) NULL,
+);
+
